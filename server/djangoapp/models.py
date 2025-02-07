e90dbca883b51e2f57473ca86b611b0fd0e874fe
Shi Tao Luo <sluo5991@gmail.com>
2025-02-07 12:59:58 -0500

    update syntax for pylint 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -14,15 +11,12 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(
-        CarMake, on_delete=models.CASCADE
-    )  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ("SEDAN", "Sedan"),
         ("SUV", "SUV"),
         ("WAGON", "Wagon"),
-        # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(
@@ -31,4 +25,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-
