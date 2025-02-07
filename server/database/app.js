66e60c5be436075ecd8b4e09d438fe93761eb3ad
Shi Tao Luo <sluo5991@gmail.com>
2025-02-07 14:11:29 -0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,4 +1,4 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -34,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+	await res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -90,8 +90,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0].id+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 		"id": new_id,
